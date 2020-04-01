a9966f8e77c569bb4209a8f529160f6a8fb7d855
Mederic <medericnetto@gmail.com>
2020-04-01 17:19:23 +0200

    Ajout du tableau de tickets

--- src/components/pages/Tickets.jsx
+++ src/components/pages/Tickets.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Data from '../../data/data.json'
+
 import Box from '@material-ui/core/Box';
 import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
@@ -13,7 +15,10 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
-//import data from 'src/data.json';
+import IconButton from '@material-ui/core/IconButton';
+import Visibility from '@material-ui/icons/Visibility';
+import Delete from '@material-ui/icons/Delete';
+
 
 
 const useStyles = makeStyles({
@@ -22,45 +27,45 @@ const useStyles = makeStyles({
     },
 });
 
-function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
 export const Tickets = (props) => {
 
     const classes = useStyles();
 
     return (
-        <TableContainer component={Paper}>
+            <TableContainer component={Paper}>
             <Table className={classes.table} aria-label="simple table">
                 <TableHead>
                     <TableRow>
                         <TableCell>#</TableCell>
-                        <TableCell align="right">!</TableCell>
-                        <TableCell align="right">Titre</TableCell>
-                        <TableCell align="right">Crée&nbsp;le</TableCell>
-                        <TableCell align="right">Catégorie</TableCell>
-                        <TableCell align="right">&nbsp;</TableCell>
+                        <TableCell align="center">!</TableCell>
+                        <TableCell align="center">Titre</TableCell>
+                        <TableCell align="center">Crée&nbsp;le</TableCell>
+                        <TableCell align="center">Catégorie</TableCell>
+                        <TableCell align="center">&nbsp;</TableCell>
+                        <TableCell align="center">&nbsp;</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {rows.map((row) => (
-                        <TableRow key={row.name}>
+                    {Data.map((row) => (
+                        <TableRow key={row._id}>
                             <TableCell component="th" scope="row">
-                                {row.name}
+                                {row.index}
                             </TableCell>
-                            <TableCell align="right">{row.calories}</TableCell>
-                            <TableCell align="right">{row.fat}</TableCell>
-                            <TableCell align="right">{row.carbs}</TableCell>
-                            <TableCell align="right">{row.protein}</TableCell>
+                            <TableCell align="center">{row.urgency}</TableCell>
+                            <TableCell align="right">{row.title}</TableCell>
+                            <TableCell align="right">{row.openDate}</TableCell>
+                            <TableCell align="center">{row.category}</TableCell>
+                            <TableCell align="center">
+                                <IconButton color="primary" aria-label="details" href={"/tickets/" + row._id}>
+                                    <Visibility />
+                                </IconButton >
+                            </TableCell>
+                            <TableCell align="center">
+                                <IconButton color="secondary" aria-label="supprimer" href={"/tickets/" + row._id}>
+                                    <Delete />
+                                </IconButton >
+                            </TableCell>
+
                         </TableRow>
                     ))}
                 </TableBody>
