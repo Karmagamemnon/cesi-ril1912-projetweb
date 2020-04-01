c9f60c037d85a21843b32e343a425ed30028ba51
Mederic <medericnetto@gmail.com>
2020-04-01 20:04:52 +0200

    Refonte de l'affichage des tickets

--- src/components/pages/Tickets.jsx
+++ src/components/pages/Tickets.jsx
@@ -4,72 +4,127 @@ import Data from '../../data/data.json'
 
 import Box from '@material-ui/core/Box';
 import Link from '@material-ui/core/Link';
-import Typography from '@material-ui/core/Typography';
 
 import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
-import IconButton from '@material-ui/core/IconButton';
-import Visibility from '@material-ui/icons/Visibility';
-import Delete from '@material-ui/icons/Delete';
+import Grid from '@material-ui/core/Grid';
+
+// import IconButton from '@material-ui/core/IconButton';
+// import Visibility from '@material-ui/icons/Visibility';
+// import Delete from '@material-ui/icons/Delete';
+import Button from '@material-ui/core/Button';
 
 
 
-const useStyles = makeStyles({
-    table: {
-        minWidth: 650,
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+        flexGrow: 1,
     },
-});
+    heading: {
+        fontSize: theme.typography.pxToRem(15),
+        flexBasis: '33.33%',
+        flexShrink: 0,
+    },
+    secondaryHeading: {
+        fontSize: theme.typography.pxToRem(15),
+        color: theme.palette.text.secondary,
+    },
+}));
 
 export const Tickets = (props) => {
-
     const classes = useStyles();
 
-    return (
-            <TableContainer component={Paper}>
-            <Table className={classes.table} aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>#</TableCell>
-                        <TableCell align="center">!</TableCell>
-                        <TableCell align="center">Titre</TableCell>
-                        <TableCell align="center">Crée&nbsp;le</TableCell>
-                        <TableCell align="center">Catégorie</TableCell>
-                        <TableCell align="center">&nbsp;</TableCell>
-                        <TableCell align="center">&nbsp;</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {Data.map((row) => (
-                        <TableRow key={row._id}>
-                            <TableCell component="th" scope="row">
-                                {row.index}
-                            </TableCell>
-                            <TableCell align="center">{row.urgency}</TableCell>
-                            <TableCell align="right">{row.title}</TableCell>
-                            <TableCell align="right">{row.openDate}</TableCell>
-                            <TableCell align="center">{row.category}</TableCell>
-                            <TableCell align="center">
-                                <IconButton color="primary" aria-label="details" href={"/tickets/" + row._id}>
-                                    <Visibility />
-                                </IconButton >
-                            </TableCell>
-                            <TableCell align="center">
-                                <IconButton color="secondary" aria-label="supprimer" href={"/tickets/" + row._id}>
-                                    <Delete />
-                                </IconButton >
-                            </TableCell>
+    // PANEL
+    const [expanded, setExpanded] = React.useState(false);
 
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+    const handleChange = (panel) => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false);
+    };
+
+    return (
+        <div className={classes.root}>
+
+            <ExpansionPanelSummary
+                aria-controls="panel1bh-content"
+                id="panel1bh-header"
+            >
+                <Grid container spacing={1}>
+                    <Grid item xs={1}>
+                        <Typography >#</Typography>
+                    </Grid>
+                    <Grid item xs={1}>
+                        <Typography >!</Typography>
+                    </Grid>
+                    <Grid item xs={8} align="center">
+                        <Typography >Title</Typography>
+                    </Grid>
+                    <Grid item xs={2}>
+                        <Typography >Category</Typography>
+                    </Grid>
+                </Grid>
+            </ExpansionPanelSummary>
+
+            {Data.map((row) => (
+                <ExpansionPanel expanded={expanded === row.index} onChange={handleChange(row.index)}>
+                    <ExpansionPanelSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1bh-content"
+                        id="panel1bh-header"
+                    >
+                        <Grid container spacing={1}>
+                            <Grid item xs={1} align="left">
+                                <Typography >{row.index}</Typography>
+                            </Grid>
+                            <Grid item xs={1} align="left">
+                                <Typography >{row.urgency}</Typography>
+                            </Grid>
+                            <Grid item xs={8} align="left">
+                                <Typography >{row.title}</Typography>
+                            </Grid>
+                            <Grid item xs={2} align="center">
+                                <Typography >{row.category}</Typography>
+                            </Grid>
+                        </Grid>
+
+                    </ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                        <div className={classes.root}>
+                            <Grid container spacing={3} >
+                                <Grid item xs={12}>
+                                    <Typography>Post by {row.requester} - {row.openDate} - For {row.tech}</Typography>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Typography>Status : {row.state}</Typography>
+                                </Grid>
+                                <Grid item xs={12} >
+                                    <Typography fontWeight="fontWeightBold" >Description :</Typography>
+                                    <Typography>{row.description}</Typography>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <Button variant="contained" color="secondary" href="#contained-buttons">
+                                        Supprimer
+                                    </Button>
+                                </Grid>
+                                <Grid item xs={6} align="right">
+                                    <Button variant="contained" color="primary" href={"/tickets/" + row._id} >
+                                        Modifier
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </div>
+
+                    </ExpansionPanelDetails>
+                </ExpansionPanel>
+            ))}
+        </div>
     );
+
 }
